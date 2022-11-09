c44e63a78aab3d72686fa8234f8dd0f69f9d5e0d
Li <lir3@AD.MEE.TCD.IE>
2022-11-09 10:32:38 +0000

    FirstCommit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Creator: Ruowei Li
+# Email: lir3@tcd.ie
+import numpy as np
+
+arrld = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(arrld)
+print(arrld.shape)
+print("\n")
+
+
+print("Hello World!")
+
+myName = "Ruowei Li"
+myAge = 23
+print(myName)
+print(myAge)
+ 
+captials = {
+   "China" : "Beijing",
+   "Ireland" : "Dublin"
+
+}
+
+print()
+def sum(a, b):
+    result = a + b
+    return result
+
+print(sum(10,100))
